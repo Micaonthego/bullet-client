e67b4339f9e777f06d08bd1262385286c817880c
github username <github email address>
2019-05-22 00:09:48 -0400

    some profile css

--- src/Profile.js
+++ src/Profile.js
@@ -3,7 +3,20 @@ import React from 'react';
 function Profile() {
     return (
 <div>
-    
+    <div className="row">
+                    <div className="profile">
+                        <figure className="profile__shape">
+                            <img src="../nat-8.jpg" alt="Profile" className="profile__img"></img>
+                            <figcaption className="profile__caption">micaonthego</figcaption>
+                        </figure>
+                        <div className="profile__text">
+                            <h3 className="heading-tertiary u-margin-bottom-small">Aspiration</h3>
+                            <p>
+                                "Be Happy"
+                            </p>
+                        </div>
+                    </div>
+                </div>
 </div>
     )
 }
