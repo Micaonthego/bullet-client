4560c7504102848d561261fd3da5937783ea5e95
github username <github email address>
2019-05-29 15:34:50 -0400

    fixed hover on timeline

--- src/Calendar.js
+++ src/Calendar.js
@@ -42,7 +42,7 @@ class Calendar extends Component {
         // console.log(this.state.bullets)
         return (
             <div>
-                <h1 style={{ color: "black" }}>CALENDAR</h1>
+                <h1 style={{ color: "black" }}>MY BULLETS</h1>
                 <div id='card-container'>
                     {this.renderBullets()}
                 </div>
--- src/TimelineBullet.js
+++ src/TimelineBullet.js
@@ -13,19 +13,19 @@ function TimelineBullet(props) {
                 <div className={props.index % 2 === 0 ? "container left" : "container right"}>
                     <div className="content">
                         <div className="card-timeline">
-                            <div className="card__side card__side--front">
-                                <div className="card__picture card__picture--1">
+                            <div className="card-timeline__side card-timeline__side--front">
+                                <div className="card-timeline__picture card__picture--1">
                                     <img className="responsive" src={props.bullet.photo} alt="">
                                     </img>
                                 </div>
-                                <h4 className="card__heading">
-                                    <span className="card__heading-span card__heading-span--1">{props.bullet.date}</span>
+                                <h4 className="card-timeline__heading">
+                                    <span className="card-timeline__heading-span card-timeline__heading-span--1">{props.bullet.date}</span>
                                 </h4>
                             </div>
-                            <div className="card__side card__side--back card__side--back-1">
-                                <div className="card__cta">
-                                    <div className="card__bullet-box">
-                                        <div className="card__details">
+                            <div className="card-timeline__side card-timeline__side--back card-timeline__side--back-1">
+                                <div className="card-timeline__cta">
+                                    <div className="card-timeline__bullet-box">
+                                        <div className="card-timeline__details">
                                             <ul>
                                                 <li>I AM GRATEFUL FOR</li>
                                                 <li>{props.bullet.gratitude}</li>
